1605093bbe973d7a70c198511e5138fefb5bc00a
qianshi <2687346406@qq.com>
2022-08-14 23:55:36 +0800

    11

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    首页
+  <div id="blogIndex">
+    <div class="title">
+      首页背景图抠字
+    </div>
+    <div class="mask">
+    </div>
   </div>
 </template>
 
@@ -8,6 +12,37 @@
 
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+#blogIndex {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: url('@/assets/111.jpg') no-repeat;
+  background-size: 100% 100%;
+  .mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: hsla(0, 0%, 100%, .4);
+    backdrop-filter: saturate(180%), blur(2);
+    z-index: 1;
+  }
+  .title {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    line-height: 100vh;
+    color: transparent;
+    background: url('@/assets/111.jpg') no-repeat;
+    background-size: 100% 100%;
+    background-clip: text;
+    font-size: 80px;
+    font-weight: 700;
+    z-index: 2;
+  }
+}
 </style>
