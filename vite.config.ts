40f741df369ee5996331dcb5369ef92898574fea
xingyijun <2687346406@qq.com>
2022-09-24 15:32:13 +0800

    更新https

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://oss.xingyijun.cn',
-        // target: 'http://localhost:3000',
+        target: 'http://localhost:3000',
         rewrite: (path) => path.replace(/^\/api/, ''),
         changeOrigin: true
       }
