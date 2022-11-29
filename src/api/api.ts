8ac4cfa84c6025dd4bea24de7524b9a19ee62518
千拾 <56419687+QianShi-ffg@users.noreply.github.com>
2022-11-29 10:02:02 +0800

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,16 +5,13 @@ import axios from './index'
 //   return axios.request({ method: 'post', url: '/articleList', data: params  })
 // }
 export const getArticleList = (params: object) => {
-  return axios.get( '/article/publishArticle', { params })
-}
-export const getArticleDetail = (params: any) => {
-  return axios.get(`/article/${params.id}`, {})
+  return axios.get( '/publishArticleList', { params })
 }
 export const aWord = (params: object) => {
   return axios.get( 'https://v1.hitokoto.cn/', { params })
 }
 export const getClassifyIdList = (params: object) => {
-  return axios.get( '/article/articleClassifyCount', { params })
+  return axios.get( '/artClassifyList', { params })
 }
 export const getFriendShipList = (params: object) => {
   return axios.get( '/friendShipList', { params })
